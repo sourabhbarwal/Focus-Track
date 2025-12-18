4402941f7350e66cc9a4b9816efd27bf696f1f03
Sourabh Barwal <sourabh17barwal@gmail.com>
2025-12-19 01:30:24 +0530

    fixed backend url.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", // later: your Render URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, // later: your Render URL
 });
